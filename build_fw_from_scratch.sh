516fbb296910db09fc7361340ecb0fc25d06b292
Anshul Yadav <anshuly@birddog.tv>
2023-05-19 06:32:23 -0400

    minor

--- build_fw_from_scratch.sh
+++ build_fw_from_scratch.sh
@@ -9,11 +9,12 @@ cd ./cam-fw-deb
 git pull origin main
 cd cam-fw-deb
 source buildfw.sh $1 $2
-echo "Finished."
+
 cd $SCRIPT_DIR
 
 if[ $3 != "" ];then
 
 sshpass -p bdstum01r21 scp -v -P 9031 ./BirdDog_CAM_$1.deb root@$3:/tmp
-
+echo "Copied to remote."
 fi
+echo "Finished."
