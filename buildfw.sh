aafd5ea489c27a09bf9e055cc4552dd63cd41aa6
Anshul Yadav <anshuly@birddog.tv>
2023-05-19 06:04:18 -0400

    minor

--- buildfw.sh
+++ buildfw.sh
@@ -8,7 +8,7 @@ fi
 Version=$1
 
 PROJECT_ROOT=`pwd`
-echo $PROJECT_ROOT
+#echo $PROJECT_ROOT
 CAM_ROOT=$PROJECT_ROOT/FW_APP_CAM
 CAM_FW_ROOT=$CAM_ROOT/firmware
 DEB_ROOT_PATH=$CAM_FW_ROOT/BirdDog_CAM_FW
@@ -16,29 +16,31 @@ BIRDDOG_PLATFORM_FILES_PATH=$DEB_ROOT_PATH/birddog-platform-files
 DEB_SOURCE_PATH=$PROJECT_ROOT/BirdDog_CAM_$Version
 DPKG_SCRIPT_PATH=$DEB_SOURCE_PATH/DEBIAN
 
-echo $DEB_SOURCE_PATH
+#echo $DEB_SOURCE_PATH
 echo "cloning gits $CAM_FW_ROOT  "
 
 git clone git@github.com:BirdDog-Australia/FW_APP_CAM.git --branch main 
 cd $CAM_ROOT && git pull origin main && cd -
-echo `pwd`
+#echo `pwd`
 git clone git@github.com:BirdDog-Australia/New_WEBUI.git --branch NDI-5 && git pull
 cd $PROJECT_ROOT/New_WEBUI && git pull origin NDI-5 && cd -
 
-echo "clond all gits $CAM_FW_ROOT  "
+echo "cloned all repositories"
 #Edit version info
 
 cp $PROJECT_ROOT/build_sil2_webui_api_rapp.sh $CAM_FW_ROOT
 cd $CAM_FW_ROOT  
-echo `pwd`
+#echo `pwd`
 
 source build_sil2_webui_api_rapp.sh $Version sil2 webui api rapp
-echo "Done building genrate bdg.fw"
+
 
 mv $CAM_FW_ROOT/BirdDog_CAM_$Version.fw $PROJECT_ROOT
 
+echo "Done building firmware at $PROJECT_ROOT/BirdDog_CAM_$Version.fw"
+
 if [ $2 == "dpkgon" ];then
-    echo "build dpkg on"
+    echo "Building debian package is ON"
     #run build_sil2_webui_api_rapp.sh $Version sil2 webui api rapp
     #To create debian package
     #copy all $\BirdDog_CAM_FW\birddog-platform-files
@@ -63,9 +65,11 @@ if [ $2 == "dpkgon" ];then
     chmod 0755 $DPKG_SCRIPT_PATH/control
     
     dpkg-deb --build $DEB_SOURCE_PATH
-    echo "Done building debian-package."
-   
+    
+    echo "Done building  debian-package at $DEB_SOURCE_PATH.deb"
     rm -rf $DEB_SOURCE_PATH
 fi
+
+echo "Finished all successfully"
 
 
