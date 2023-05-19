e6c2430ae54163f47d92d54d69f3b47242518352
Anshul Yadav <anshuly@birddog.tv>
2023-05-19 08:07:52 -0400

    sshpass

--- build_fw_from_scratch.sh
+++ build_fw_from_scratch.sh
@@ -4,17 +4,15 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 echo $SCRIPT_DIR
 cd $SCRIPT_DIR
 
-git clone git@github.com:AnshulYbd/cam-fw-deb.git
-cd ./cam-fw-deb
-git pull origin main
-cd cam-fw-deb
-source buildfw.sh $1 $2
 
-cd $SCRIPT_DIR
+echo $3
+VAR=$3
+if [ -n "$VAR" ]; then
+  command="-p bdstum01r21 scp -v -P 9031 $SCRIPT_DIR/cam-fw-deb/BirdDog_CAM_$1.deb root@$VAR:/tmp"
+  echo $command
+  sshpass $command 
+  echo "Copied to remote."
 
-if[ $3 != "" ];then
-
-sshpass -p bdstum01r21 scp -v -P 9031 ./BirdDog_CAM_$1.deb root@$3:/tmp
-echo "Copied to remote."
 fi
+
 echo "Finished."
